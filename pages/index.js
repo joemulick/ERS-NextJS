d719a194082830186873ed7ea6f28824b9de2a32
JoeMulick <joemulick@gmail.com>
2018-03-26 22:36:13 -0700

    Semantic-UI-React functioning via CDN in head

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
+import Head from 'next/head'
 import Layout from '../components/MyLayout.js'
 import Link from 'next/link'
-
+import { Button } from 'semantic-ui-react'
 
 const PostLink = (props) => (
   <li>
@@ -11,12 +12,19 @@ const PostLink = (props) => (
 )
 
 export default () => (
-  <Layout>
-    <h1>My Blog</h1>
-    <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js"/>
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
-    </ul>
-  </Layout>
+  <div>
+    <Head>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+    </Head>
+
+    <Layout>
+      <h1>My Blog</h1>
+      <Button>Click Here</Button>
+      <ul>
+        <PostLink id="hello-nextjs" title="Hello Next.js"/>
+        <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
+        <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
+      </ul>
+    </Layout>
+  </div>
 )
